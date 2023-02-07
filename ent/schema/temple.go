c3e264bf6acf28b9cf67e8274f5a4e1665ed785d
hewenyu <yuebanlaosiji@outlook.com>
2023-02-07 05:53:03 +0000

    add

--- ent/schema/temple.go
+++ ent/schema/temple.go
@@ -1,14 +1,17 @@
 package schema
 
 import (
-	"time"
-
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	"github.com/hewenyu/todo/ent/mixins"
 )
 
 // Temple holds the schema definition for the Temple entity.
+// 这是一个模板表
 type Temple struct {
 	ent.Schema
 }
@@ -21,21 +24,6 @@ func (Temple) Fields() []ent.Field {
 			Annotations(
 				entgql.OrderField("TEXT"),
 			),
-		field.Time("created_at").
-			Default(time.Now).
-			Immutable().
-			Annotations(
-				entgql.OrderField("CREATED_AT"),
-			),
-		field.Enum("status").
-			NamedValues(
-				"InProgress", "IN_PROGRESS",
-				"Completed", "COMPLETED",
-			).
-			Default("IN_PROGRESS").
-			Annotations(
-				entgql.OrderField("STATUS"),
-			),
 		field.Int("priority").
 			Default(0).
 			Annotations(
@@ -46,5 +34,28 @@ func (Temple) Fields() []ent.Field {
 
 // Edges of the Temple.
 func (Temple) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("parent", Temple.Type).
+			Unique().
+			From("children"),
+		edge.From("owner", User.Type).
+			Ref("todos").
+			Unique(),
+	}
+}
+
+// 加载公共参数
+func (Temple) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixins.BaseMixin{},
+	}
+}
+
+// 定义表相关信息
+func (Temple) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "temple"},
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
 }
--- ent/schema/user.go
+++ ent/schema/user.go
@@ -1,6 +1,13 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"github.com/hewenyu/todo/ent/mixins"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -14,5 +21,23 @@ func (User) Fields() []ent.Field {
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("todos", Temple.Type),
+	}
+}
+
+// 加载公共参数
+func (User) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixins.BaseMixin{},
+	}
+}
+
+// 定义表相关信息
+func (User) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "user"},
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
 }
