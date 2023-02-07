a6b66100b4c43caf46b537c60e377ff48bd50ae8
hewenyu <yuebanlaosiji@outlook.com>
2023-02-07 07:50:11 +0000

    add

--- ent/entc.go
+++ ent/entc.go
@@ -1,29 +1,29 @@
-package main
+package ent
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"entgo.io/contrib/entgql"
-	"entgo.io/ent/entc"
-	"entgo.io/ent/entc/gen"
-)
+// 	"entgo.io/contrib/entgql"
+// 	"entgo.io/ent/entc"
+// 	"entgo.io/ent/entc/gen"
+// )
 
-func main() {
-	ex, err := entgql.NewExtension(
-		entgql.WithWhereFilters(true),
-		entgql.WithConfigPath("../gqlgen.yml"),
-		// Generate the filters to a separate schema
-		// file and load it in the gqlgen.yml config.
-		entgql.WithSchemaPath("../ent.graphql"),
-	)
-	if err != nil {
-		log.Fatalf("creating entgql extension: %v", err)
-	}
-	opts := []entc.Option{
-		entc.Extensions(ex),
-		entc.TemplateDir("./template"),
-	}
-	if err := entc.Generate("./schema", &gen.Config{}, opts...); err != nil {
-		log.Fatalf("running ent codegen: %v", err)
-	}
-}
+// func main() {
+// 	ex, err := entgql.NewExtension(
+// 		entgql.WithWhereFilters(true),
+// 		entgql.WithConfigPath("../gqlgen.yml"),
+// 		// Generate the filters to a separate schema
+// 		// file and load it in the gqlgen.yml config.
+// 		entgql.WithSchemaPath("../ent.graphql"),
+// 	)
+// 	if err != nil {
+// 		log.Fatalf("creating entgql extension: %v", err)
+// 	}
+// 	opts := []entc.Option{
+// 		entc.Extensions(ex),
+// 		// entc.TemplateDir("./template"),
+// 	}
+// 	if err := entc.Generate("./schema", &gen.Config{}, opts...); err != nil {
+// 		log.Fatalf("running ent codegen: %v", err)
+// 	}
+// }
